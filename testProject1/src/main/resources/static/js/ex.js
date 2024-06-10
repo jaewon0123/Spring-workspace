489bbe9cdc47dfeefb0047540ff888f1102e64ec
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-10 17:53:01 +0900

    servlet

---
+++ testProject1/src/main/resources/static/js/ex.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+<script
+      type="text/javascript"
+      src="http://code.jquery.com/jquery-3.7.1.js"
+    ></script>
+    
+  document.querySelector("#btn").addEventListener("click",()=>{
+	alert("클릭됨");
+ })
+
+ 
