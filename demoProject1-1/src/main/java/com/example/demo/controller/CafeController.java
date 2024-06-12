74f450859e26aedeb6d6ede0c20d10a6294b68bf
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-12 17:22:10 +0900

    List DTO log

--- demoProject1-1/src/main/java/com/example/demo/controller/CafeController.java
+++ demoProject1-1/src/main/java/com/example/demo/controller/CafeController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.model.dto.CafeDTO;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("cafe")
 @Slf4j
 public class CafeController {
-
+/*
 	@PostMapping("cafe-index")
 	public String cafeMainComment(@RequestParam("cafeCommentName")String cafeCommentName,
 						@RequestParam("cafeCommentOpinion")String cafeCommentOpinion) {
@@ -21,5 +23,17 @@ public class CafeController {
 		log.debug("cafeCommentOpinion : " + cafeCommentOpinion);
 		
 		return "redirect:/cafe/cafe-index";
+	}
+	*/
+	
+	public String cafeIndexComment(CafeDTO cafeDTO) {
+		
+		CafeDTO cd = new CafeDTO();
+		cd.getCafeCommentName();
+		cd.getCafeCommentOpinion();
+		
+		cd.setCafeCommentName("넣어줄값 = 나중에 db연결해서 전달해줄 값 넣어줄 것");
+		cd.setCafeCommentOpinion("넣어줄값 = 나중에 db연결해서 전달해줄 값 넣어줄 것");
+		return "";
 	}
 }
