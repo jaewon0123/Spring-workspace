d42267584dbee84235d00ccd7ad9bd5895f0885d
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-13 17:28:56 +0900

    List Model

---
+++ demoProject2/src/main/java/com/example/demo/dto/MemberDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+
+	private String memberNo;
+	private String memberName;
+	private int memberAge;
+}
