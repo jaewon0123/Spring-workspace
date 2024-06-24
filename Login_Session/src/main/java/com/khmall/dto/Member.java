c708fcf35de3a391bb662899851c0c05882e4336
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-24 12:38:35 +0900

    login Map

---
+++ Login_Session/src/main/java/com/khmall/dto/Member.java
@@ -0,0 +1,15 @@
+package com.khmall.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Member {
+	private int member_id;
+	private String member_name;
+	private int member_age;
+	private String member_phone;
+}
