019e61c7ed8b694eeecf006666836170d2715743
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-26 12:29:10 +0900

    로그인 코드 설명

--- Login_MyPage/src/main/java/com/khmall/controller/LoginController.java
+++ Login_MyPage/src/main/java/com/khmall/controller/LoginController.java
@@ -79,18 +79,50 @@ public class LoginController {
 		model.addAttribute("member", member);
 		return "modifyProfile";
 	}
-	
+	/*
+	 * Member updateMember 와 Member member
+	 * Member updateMember 는 소비자(클라이언트)가 새롭게 작성하고 DB에 덮어쓸 내용이 임시저정
+	 * Member member 는 소비자(클라이언트)가 기존에 DB에 저장한 값
+	 * 
+	 * Member member = (Member) session.getAttribute("loginSession");
+	 * 			└── session에서 loginSession 이라는 변수명에 저장된 로그인 정보를 가져오기
+	 * 				가져와서 member 기존에 DB에 저장된 값을 불러오기
+	 * 
+	 * updateMember.setMember_id(member.getMember_id()); -- 덮어쓰기
+	 * 			└── DB에 저장된 값 중 id는 개발자가 회원가입을 한 순번을 소비자한테 부여한 순서값으로
+	 * 				소비자가 가입한 순서는 변경할 수 없기 때문에(소비자에의해 변경될 수 없기 때문에)
+	 * 				id값으로 소비자가 새롭게 input 창 안에 수정해서 작성한 값을 가져와서 임시저장
+	 * 
+	 * 			setMember_id		member.getMember_id()
+	 * 			
+	 * 			member.getMember_id() -- 기존에 DB에 저장된 정보
+	 *  		setMember_id	-- 새롭게 DB에 저장할 정보
+	 *  						아이디(회원가입순서) 는 똑같지만 밑 내용은 다를 수 있음
+	 *  
+	 *	memberService.updateMember(updateMember);
+	 *			└── 덮어쓰기 한 내용을 DB에 저장하기
+	 *			
+	 *	session.setAttribute("loginSession", updateMember);
+	 *			└── 새롭게 DB에 저장된 내용을 loginSession 이라는 변수명에 다시 저장
+	 *	
+	 */
 	@PostMapping("/modifyProfile")//form action에 작성한 주소값 넣기
+												// 새롭게 저장할 내용을 담을 공간이름
 	public String updateMember(HttpSession session, Member updateMember) {
 		//현재 로그인이 된 세션의 정보를 가지고 와서 멤버 정보 조회하는 코드
+		
+		//기존에 DB에 저장된 정보 가져오기
 		Member member = (Member) session.getAttribute("loginSession");
+		
 				
 		//만약에 로그인이 되어있지 않은데 접속하려한다. 그러면 묻지도말고 바로 홈페이지로 돌려보내자
 		if(member == null) {
 			return "redirect:/login";
 		}		
 		
+		// input에 사용자가 작성한 값을 모두 가져오기
 		updateMember.setMember_id(member.getMember_id());
+		// 기존에 있던 DB에 새롭게 수정한 내용을 덮어쓰기
 		memberService.updateMember(updateMember);
 		session.setAttribute("loginSession", updateMember);
 		return "redirect:/myPage";
@@ -118,6 +150,20 @@ public class LoginController {
 	public String showSearchFor(Model model) {
 		return "search";
 	}
+	
+	/*
+	 * @RequestParam("keyword") String keyword
+	 * 
+	 * <input type="text" name="keyword" placeholder="이름 또는 전화번호 입력" required>
+	 * 
+	 * @RequestParam("input이나 태그에 작성한 name 또는 th로 작성된 변수명") String keyword
+	 * input에서 name = "keyword" 이기 때문에 @RequestParam("keyword") 쓴 것
+	 * input에서 name , th:field 로 작성된 변수명과 무조건 일치 해야함 @RequestParam("  ")
+	 * 
+	 * String keyword 는 input 에서 바라보는 keyword라는 값을 가져와서
+	 * 자바에서 가져온 값을 담을 공간
+	 * 
+	 */
 	
 	//검색하는 결과 볼 수 있도록 @PostMapping
 	@PostMapping("/search")
